63facd793d6f272341483db19dd04fb5fa5777f2
vitateje <47430621+vitateje@users.noreply.github.com>
2022-09-13 17:36:53 -0300

    Update base_api.rb

--- api/spec/routes/base_api.rb
+++ api/spec/routes/base_api.rb
@@ -2,5 +2,5 @@ require "httparty"
 
 class BaseApi
   include HTTParty
-  base_uri "https://desafioonline.webmotors.com.br:443"
+  base_uri "https://desafioonline.site-de-automoveis.com.br:443"
 end
